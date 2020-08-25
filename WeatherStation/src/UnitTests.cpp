7f6eeb10c0b5dd14bc7216b5ff36186bbc204bd3
gskidmor <gskidmor@ucsc.edu>
2020-08-25 16:19:58 -0700

    Recommiting due to failed add

---
+++ WeatherStation/src/UnitTests.cpp
@@ -0,0 +1,55 @@
+#include "WeatherStation.h"
+#include "UnitTests.h"
+
+//Logging
+int D_Log_Test(const char* inputFile) {
+	// Initial Hardcoded class
+	D_Class WeatherData(0, 14, 1000, 27, 10, 0);
+
+	// Calling the log method
+	WeatherData.Log(std::move(inputFile));
+
+	std::ifstream dataFile(inputFile);
+	// Now we just read the file
+	std::string line, data, lastLine;
+	int lineCounter = 0;
+	while (std::getline(dataFile, line)) {
+		if (!lineCounter) {
+			data += line;
+		}
+		else {
+			data += "\n" + line;
+		}
+		lineCounter++;
+	}
+	// Now that we have the full file in data, we find the last '\n' char and read everything after it
+	unsigned int startingIndex = data.find_last_of('\n') + 1;
+	// With this, we are looking to place everything in the last line into lastLine
+	for (unsigned int oldIndex = startingIndex; oldIndex < data.length(); oldIndex++) {
+		lastLine += data[oldIndex];
+	}
+
+	// Finally we compare with what was read in vs our hardcoded value
+	return((lastLine.compare("0.000000,14.000000,1000,27,1") == 0) ? 1 : 0);
+}
+
+
+// Pulling
+int P_Pull_Test(const char* inputFile) {
+	P_Class PullData;
+	PullData.Clear();
+
+	PullData.Pull(std::move(inputFile));
+
+	std::ifstream pullFile(inputFile);
+	std::string data;
+	if (pullFile.is_open()) {
+		// Here we pull the first line from the file, which should be the only line
+		std::getline(pullFile, data);
+		pullFile.close();
+	}
+
+	// Finally we compare with what was read in vs the hardcoded value found in "NN_Guess_TEST.txt"
+	return((data.compare("001110") == 0) ? 1 : 0);
+}
+	
---
+++ WeatherStation/src/UnitTests.h
@@ -0,0 +1,13 @@
+#ifndef UNIT_TESTING
+#define UNIT_TESTING
+#include "WeatherStation.h"
+
+
+
+// Function Prototypes
+int D_Log_Test(const char*);
+
+
+int P_Pull_Test(const char*);
+
+#endif //UNIT_TESTING
